49d65ef6bc7203d0b6697ffc4d85cded367614fa
Victor Macedo <victoralexandrino99@gmail.com>
2022-04-12 21:30:40 -0300

    update job

--- marvel-safra/js/controllers/hero/hero-controller.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  var as = angular.module('HeroMarvel', ['ngRoute']);
-
-  as.controller('HeroCtrl', function ($scope, $location, $http) {
-    $scope.teste = "testeee";
-
-  });
-}());
--- marvel-safra/js/services/app-service.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    var as = angular.module('app.services', ['ngRoute']);
-
-    as.factory('AppService', ['$q', '$http', '$rootScope', '$location', 'CONSTANTS']);
-
-    as.factory('AppService', ['$q', '$http', '$rootScope', '$location', 'CONSTANTS', function ($q, $http, $rootScope, $location, $cookieStore, CONSTANTS) {
-
-    }]);
-}());
